e4dc5fb1d641a98cebfae520cdeab13ca8dbcee4
Brunngard1994 <32332194+Brunngard1994@users.noreply.github.com>
2018-01-24 23:44:49 +0100

    Add marker to map

--- js/game.js
+++ js/game.js
@@ -289,10 +289,10 @@ var style_spy = [
     ]
   }
   ];
-  
+
   var styled_spy = new google.maps.StyledMapType(style_spy, {name: "spy style"});
-  
-  
+
+
   //Create the variables that will be used within the map configuration options.
   //The latitude and longitude of the center of the map.
   var spyMapCenter = new google.maps.LatLng(59.313289, 18.110288);
@@ -301,12 +301,12 @@ var style_spy = [
   //The max and min zoom levels that are allowed.
   var spyMapZoomMax = 21;
   var spyMapZoomMin = 6;
-  
-  //These options configure the setup of the map. 
-  var spyMapOptions = { 
-        center: spyMapCenter, 
+
+  //These options configure the setup of the map.
+  var spyMapOptions = {
+        center: spyMapCenter,
             zoom: spyMapZoom,
-        //The type of map. In addition to ROADMAP, the other 'premade' map styles are SATELLITE, TERRAIN and HYBRID. 
+        //The type of map. In addition to ROADMAP, the other 'premade' map styles are SATELLITE, TERRAIN and HYBRID.
             mapTypeId: google.maps.MapTypeId.ROADMAP,
         maxZoom:spyMapZoomMax,
         minZoom:spyMapZoomMin,
@@ -314,51 +314,54 @@ var style_spy = [
         panControl: false,
         mapTypeControl: false,
   };
-  
+
   //Create the variable for the main map itself.
   var spyMap;
-  
+
   //When the page loads, the line below calls the function below called 'loadspyMap' to load up the map.
   google.maps.event.addDomListener(window, 'load', loadspyMap);
-  
-  
-  
+
+
+
   //THE MAIN FUNCTION THAT IS CALLED WHEN THE WEB PAGE LOADS --------------------------------------------------------------------------------
   function loadspyMap() {
-    
-  //The empty map variable ('spyMap') was created above. The line below creates the map, assigning it to this variable. The line below also loads the map into the div with the id 'spy-map' (see code within the 'body' tags below), and applies the 'spyMapOptions' (above) to configure this map. 
-  spyMap = new google.maps.Map(document.getElementById("spyMap"), spyMapOptions);	
-  
-    
+
+  //The empty map variable ('spyMap') was created above. The line below creates the map, assigning it to this variable. The line below also loads the map into the div with the id 'spy-map' (see code within the 'body' tags below), and applies the 'spyMapOptions' (above) to configure this map.
+  spyMap = new google.maps.Map(document.getElementById("spyMap"), spyMapOptions);
+
+
   spyMap.mapTypes.set('map_styles_spy', styled_spy);
   spyMap.setMapTypeId('map_styles_spy');
-  
-  
+
+
   //Calls the function below to load up all the map markers.
   loadMapMarkers();
-  
+
   }
-  
-  
-  
+
+ 
+
+
+
+
   //Function that loads the map markers.
   function loadMapMarkers (){
-  
+
   //GLASTONBURY -----------------
-  
+
   //Setting the position of the Glastonbury map marker.
   var markerPositionGlastonbury = new google.maps.LatLng(59.313289, 18.110288);
-  
-  
+
+
   //Creating the Glastonbury map marker.
   markerGlastonbury = new google.maps.Marker({
         //uses the position set above.
       position: markerPositionGlastonbury,
       //adds the marker to the map.
         map: spyMap,
+        icon: '../img/usa.pin.png',
         title: 'KYH',
   });
-  
-  
-  }
 
+
+  }
