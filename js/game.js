cbae07d421dfbd8a7c4a91e5093e415b38b8c775
Virrevirre <vike.pet@hotmail.com>
2018-01-24 13:31:59 +0100

    Replace map js code. Clean up the html and css files. Make map track current position when the player is moving.

--- js/game.js
+++ js/game.js
@@ -1,364 +1,54 @@
-var style_spy = [
-  {
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#1d2c4d"
-      }
-    ]
-  },
-  {
-    "elementType": "labels",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#8ec3b9"
-      }
-    ]
-  },
-  {
-    "elementType": "labels.text.stroke",
-    "stylers": [
-      {
-        "color": "#1a3646"
-      }
-    ]
-  },
-  {
-    "featureType": "administrative.country",
-    "elementType": "geometry.stroke",
-    "stylers": [
-      {
-        "color": "#616161"
-      }
-    ]
-  },
-  {
-    "featureType": "administrative.land_parcel",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "administrative.land_parcel",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#64779e"
-      }
-    ]
-  },
-  {
-    "featureType": "administrative.neighborhood",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "administrative.province",
-    "elementType": "geometry.stroke",
-    "stylers": [
-      {
-        "color": "#4b6878"
-      }
-    ]
-  },
-  {
-    "featureType": "landscape.man_made",
-    "elementType": "geometry.stroke",
-    "stylers": [
-      {
-        "color": "#334e87"
-      }
-    ]
-  },
-  {
-    "featureType": "landscape.natural",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#023e58"
-      }
-    ]
-  },
-  {
-    "featureType": "poi",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#283d6a"
-      }
-    ]
-  },
-  {
-    "featureType": "poi",
-    "elementType": "labels.text",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "poi",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#6f9ba5"
-      }
-    ]
-  },
-  {
-    "featureType": "poi",
-    "elementType": "labels.text.stroke",
-    "stylers": [
-      {
-        "color": "#1d2c4d"
-      }
-    ]
-  },
-  {
-    "featureType": "poi.business",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "poi.park",
-    "elementType": "geometry.fill",
-    "stylers": [
-      {
-        "color": "#023e58"
-      }
-    ]
-  },
-  {
-    "featureType": "poi.park",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#3C7680"
-      }
-    ]
-  },
-  {
-    "featureType": "road",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#8e0000"
-      }
-    ]
-  },
-  {
-    "featureType": "road",
-    "elementType": "labels.icon",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "road",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#98a5be"
-      }
-    ]
-  },
-  {
-    "featureType": "road",
-    "elementType": "labels.text.stroke",
-    "stylers": [
-      {
-        "color": "#1d2c4d"
-      }
-    ]
-  },
-  {
-    "featureType": "road.highway",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#8e0000"
-      }
-    ]
-  },
-  {
-    "featureType": "road.highway",
-    "elementType": "geometry.stroke",
-    "stylers": [
-      {
-        "color": "#8e0000"
-      }
-    ]
-  },
-  {
-    "featureType": "road.highway",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#b0d5ce"
-      }
-    ]
-  },
-  {
-    "featureType": "road.highway",
-    "elementType": "labels.text.stroke",
-    "stylers": [
-      {
-        "color": "#023e58"
-      }
-    ]
-  },
-  {
-    "featureType": "transit",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "transit",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#98a5be"
-      }
-    ]
-  },
-  {
-    "featureType": "transit",
-    "elementType": "labels.text.stroke",
-    "stylers": [
-      {
-        "color": "#1d2c4d"
-      }
-    ]
-  },
-  {
-    "featureType": "transit.line",
-    "elementType": "geometry.fill",
-    "stylers": [
-      {
-        "color": "#283d6a"
-      }
-    ]
-  },
-  {
-    "featureType": "transit.station",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#3a4762"
-      }
-    ]
-  },
-  {
-    "featureType": "water",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#0e1626"
-      }
-    ]
-  },
-  {
-    "featureType": "water",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#4e6d70"
-      }
-    ]
-  }
-  ];
-  
-  var styled_spy = new google.maps.StyledMapType(style_spy, {name: "spy style"});
-  
-  
-  //Create the variables that will be used within the map configuration options.
-  //The latitude and longitude of the center of the map.
-  var spyMapCenter = new google.maps.LatLng(59.313289, 18.110288);
-  //The degree to which the map is zoomed in. This can range from 0 (least zoomed) to 21 and above (most zoomed).
-  var spyMapZoom = 15;
-  //The max and min zoom levels that are allowed.
-  var spyMapZoomMax = 21;
-  var spyMapZoomMin = 6;
-  
-  //These options configure the setup of the map. 
-  var spyMapOptions = { 
-        center: spyMapCenter, 
-            zoom: spyMapZoom,
-        //The type of map. In addition to ROADMAP, the other 'premade' map styles are SATELLITE, TERRAIN and HYBRID. 
-            mapTypeId: google.maps.MapTypeId.ROADMAP,
-        maxZoom:spyMapZoomMax,
-        minZoom:spyMapZoomMin,
-        //Turn off the map controls as we will be adding our own later.
-        panControl: false,
-        mapTypeControl: false,
-  };
-  
-  //Create the variable for the main map itself.
-  var spyMap;
-  
-  //When the page loads, the line below calls the function below called 'loadspyMap' to load up the map.
-  google.maps.event.addDomListener(window, 'load', loadspyMap);
-  
-  
-  
-  //THE MAIN FUNCTION THAT IS CALLED WHEN THE WEB PAGE LOADS --------------------------------------------------------------------------------
-  function loadspyMap() {
-    
-  //The empty map variable ('spyMap') was created above. The line below creates the map, assigning it to this variable. The line below also loads the map into the div with the id 'spy-map' (see code within the 'body' tags below), and applies the 'spyMapOptions' (above) to configure this map. 
-  spyMap = new google.maps.Map(document.getElementById("spyMap"), spyMapOptions);	
-  
-    
-  spyMap.mapTypes.set('map_styles_spy', styled_spy);
-  spyMap.setMapTypeId('map_styles_spy');
-  
-  
-  //Calls the function below to load up all the map markers.
-  loadMapMarkers();
-  
-  }
-  
-  
-  
-  //Function that loads the map markers.
-  function loadMapMarkers (){
-  
-  //GLASTONBURY -----------------
-  
-  //Setting the position of the Glastonbury map marker.
-  var markerPositionGlastonbury = new google.maps.LatLng(59.313289, 18.110288);
-  
-  
-  //Creating the Glastonbury map marker.
-  markerGlastonbury = new google.maps.Marker({
-        //uses the position set above.
-      position: markerPositionGlastonbury,
-      //adds the marker to the map.
-        map: spyMap,
-        title: 'KYH',
-  });
-  
-  
-  }
+function startMap () {
+  var myPos = navigator.geolocation.watchPosition(initMap);
+}
 
+function initMap (myPos) {
+var MapCenter = new google.maps.LatLng(myPos.coords.latitude, myPos.coords.longitude);
+var MapZoom = 18;
+var MapZoomMax = 24;
+var MapZoomMin = 12;
+
+var mapOptions = { 
+  center: MapCenter, 
+  zoom: MapZoom,
+  //The type of map. In addition to ROADMAP, the other 'premade' map styles are SATELLITE, TERRAIN and HYBRID. 
+  mapTypeId: google.maps.MapTypeId.ROADMAP,
+  maxZoom:MapZoomMax,
+  minZoom:MapZoomMin,
+  //Turn off the map controls as we will be adding our own later.
+  panControl: false,
+  mapTypeControl: false,
+};
+
+myMap = new google.maps.Map(document.getElementById("spyMap"), mapOptions);	
+runMap(MapCenter);
+}
+
+google.maps.event.addDomListener(window, 'load', startMap);
+//var myLatlng = {lat: myPos.coords.latitude, lng: myPos.coords.longitude};
+function newMarker(pos) {
+  var marker = new google.maps.Marker({
+      position: pos,
+      map: myMap,
+      title: 'Click to zoom'
+    });
+}
+
+
+function runMap (MapCenter) {
+var marker = new google.maps.Marker({
+  position: MapCenter,
+  map: myMap,
+  title: 'Click to zoom'
+});
+}
+
+// const e = React.createElement;
+
+// ReactDOM.render(
+//     e('a', { href: "https://google.se" },
+//       e('img', { src: "https://imgs.xkcd.com/comics/random_number.png"})
+//   ),
+//     document.getElementById('react')
+
+// );
