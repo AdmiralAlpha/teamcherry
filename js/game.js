f0651655e4da325371747f5346d12d6fea9c4e9e
Virrevirre <vike.pet@hotmail.com>
2018-02-08 11:19:10 +0100

    Clean up code

--- js/game.js
+++ js/game.js
@@ -13,6 +13,40 @@ var links = [ '../index.html', 'sovprofile.html', 'usprofile.html' ];
 
 var i = 0;
 
+var x = location.search;
+console.log(x);
+console.log(window.location.search);
+
+
+function getUrlParameter(name) {
+  name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+  var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+  var results = regex.exec(location.search);
+  return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+};
+
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function updateQueryStringParameter(uri, key, value) {
+  var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
+  var separator = uri.indexOf('?') !== -1 ? "&" : "?";
+  if (uri.match(re)) {
+    return uri.replace(re, '$1' + key + "=" + value + '$2');
+  }
+  else {
+    return uri + separator + key + "=" + value;
+  }
+}
+
+
 function error(err) {
   console.warn('ERROR(' + err.code + '): ' + err.message);
 }
